61eb24ac6797d58904ef205a95b2ee621b32a9cd
Julio Pedro López Bayón <jplbayon@gmail.com>
2023-03-24 11:36:09 +0100

    update

---
+++ src/dam/psp/ex20230315/Servidor.java
@@ -0,0 +1,13 @@
+package dam.psp.ex20230315;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Servidor {
+
+	public static void main(String[] args) {
+		System.out.println(LocalDate.now().toString());
+		System.out.println(LocalTime.now().toString());
+	}
+
+}
